94276db01781ade894850a3c862c468eb107933a
kgriggs <gri18026@byui.edu>
2020-03-25 15:43:33 -0700

    commit

--- w10/create-a-sliding-menu/js.js
+++
@@ -1,5 +0,0 @@
-function toggleNav() {
-    var updateElement = document.getElementById("side-menu");
-    //toggle adds a class if it's not there or removes it if it is.
-    updateElement.classList.toggle("open");
-}
